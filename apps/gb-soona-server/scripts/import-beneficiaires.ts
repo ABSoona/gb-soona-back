5370c29bb0dc2a67b4ce8513106352609a3f3b24
kcharraq <karim.charraqua.ext@lafarge.com>
2025-11-14 20:13:06 +0100

    last updated

--- apps/gb-soona-server/scripts/import-beneficiaires.ts
+++ apps/gb-soona-server/scripts/import-beneficiaires.ts
@@ -40,7 +40,7 @@ async function resyncSequence(table: string) {
 }
 
 async function importContacts() {
-  const filePath = '/app/scripts/data/contacts.csv';
+  const filePath = './data/contacts.csv';
 
   console.log('📥 Lecture du fichier contacts.csv...');
 
